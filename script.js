9938005a27c7817c7896277972ccf8496cf17c50
rohittgangawane <128308792+rohittgangawane@users.noreply.github.com>
2023-03-27 13:04:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//JS
+const inputBox = document.querySelector('.inputBox');
+const clearBtn = document.querySelector('.clearBtn');
+const deleteBtn = document.querySelector('.deleteBtn');
+const equleBtn = document.querySelector('.equleBtn');
+
+const calculation = (e) => {
+    inputBox.value = inputBox.value += e;
+}
+
+clearBtn.onclick = () => {
+    inputBox.value = "";
+}
+deleteBtn.onclick = () => {
+    inputBox.value = inputBox.value.slice(0, -1);
+}
+
+equleBtn.onclick = () => {
+    if(inputBox.value.length >= 1) {
+        try{
+            inputBox.value = eval(inputBox.value);
+        }
+        catch(err) {
+            alert("Invalid Value");
+        }
+    } else{
+        alert("Please Add Some Value First");
+    }
+}
